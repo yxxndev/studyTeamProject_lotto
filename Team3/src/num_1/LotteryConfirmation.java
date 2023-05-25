2f62777cf2cd2439678147a3e71e592551b0f2d9
Jisu-Seong <sjangela0615@gmail.com>
2023-05-25 22:55:17 +0900

    당첨번호 임시 출력 메소드 추가

--- Team3/src/num_1/LotteryConfirmation.java
+++ Team3/src/num_1/LotteryConfirmation.java
@@ -5,7 +5,11 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
-public class LotteryConfirmation {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class LotteryConfirmation extends JFrame{
 	// 당첨번호 6개, 보너스번호 1개
 	Set<Integer> lotteryNums = new HashSet<Integer>();
 	int bonusNum;
@@ -29,5 +33,38 @@ public class LotteryConfirmation {
 	int generateRanNum() {
 		Random r = new Random();
 		return r.nextInt(45) + 1;
+	}
+
+	// 당첨번호 출력(임시)
+	void printlotteryNums() {
+		JPanel pnl = new JPanel();
+		JLabel lblLotteryNum1 = new JLabel("");
+		JLabel lblLotteryNum2 = new JLabel("");
+		JLabel lblLotteryNum3 = new JLabel("");
+		JLabel lblLotteryNum4 = new JLabel("");
+		JLabel lblLotteryNum5 = new JLabel("");
+		JLabel lblLotteryNum6 = new JLabel("");
+		JLabel lblPlusSymbol = new JLabel("+");
+		JLabel lblLotteryNum7 = new JLabel("");
+		JLabel[] lbls = { lblLotteryNum1, lblLotteryNum2, lblLotteryNum3, lblLotteryNum4, lblLotteryNum5,
+				lblLotteryNum6 };
+
+		int i = 0;
+		for (Integer elem : lotteryNums) {
+			String strPlus = String.valueOf(elem);
+			lbls[i].setText(strPlus);
+			pnl.add(lbls[i]);
+			i++;
+		}
+		lblLotteryNum7.setText(String.valueOf(bonusNum));
+		
+		pnl.add(lblPlusSymbol);
+		pnl.add(lblLotteryNum7);
+
+		add(pnl);
+
+		setSize(500, 500);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
 	}
 }
