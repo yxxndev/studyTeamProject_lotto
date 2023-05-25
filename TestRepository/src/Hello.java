2aa8b19fa51d628d2273d6c6bf119173c7a91a0d
Jisu-Seong <sjangela0615@gmail.com>
2023-05-25 11:46:31 +0900

    헤드 정리

--- TestRepository/src/Hello.java
+++ TestRepository/src/Hello.java
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 // 김지윤
-=======
 // 이정빈
->>>>>>> branch 'master' of https://github.com/0gon/TeamTestRepository.git
 // 성지수
 public class Hello {
 	public static void main(String[] args) {
