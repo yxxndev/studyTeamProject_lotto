ecfb58986a36599bc00f3d82c1cb638b49e8e0fe
yxxndev <yxxn.dev@gmail.com>
2023-05-25 11:43:07 +0900

    김지윤 이름 추가

--- TestRepository/src/Hello.java
+++ TestRepository/src/Hello.java
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 // 김지윤
-=======
 // 성지수
->>>>>>> branch 'master' of https://github.com/0gon/TeamTestRepository.git
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("반갑습니다!");
