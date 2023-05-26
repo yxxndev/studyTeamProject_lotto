3cb27aad9ac9899d0742656f1d2a20a0e9b180e6
0gon <dudrhs4563@naver.com>
2023-05-26 14:59:58 +0900

    임시

--- Team3/src/num_1/LottoCMR.java
+++ Team3/src/num_1/LottoCMR.java
@@ -19,7 +19,6 @@ public class LottoCMR extends JFrame {
 
 	ImageIcon icon = new ImageIcon("paper.png");
 	private JPanel contentPane;
-	DataBase dataBase;
 	
 	public LottoCMR(DataBase dataBase) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -41,27 +40,27 @@ public class LottoCMR extends JFrame {
 		
 		JLabel lblMarking = new JLabel("New label");
 		lblMarking.setIcon(new ImageIcon(LottoCMR.class.getResource("/image/marking.png")));
-		lblMarking.setBounds(263, 110, 20, 28);
+		lblMarking.setBounds(166, 111, 20, 28);
 		panel_3.add(lblMarking);
 		
 		JLabel lbl3 = new JLabel("3");
 		lbl3.setIcon(new ImageIcon(LottoCMR.class.getResource("/image/03.png")));
-		lbl3.setBounds(309, 67, 20, 28);
+		lbl3.setBounds(307, 67, 20, 28);
 		panel_3.add(lbl3);
 		
 		JLabel lbl2 = new JLabel("2");
 		lbl2.setIcon(new ImageIcon(LottoCMR.class.getResource("/image/02.png")));
-		lbl2.setBounds(288, 67, 20, 28);
+		lbl2.setBounds(285, 67, 20, 28);
 		panel_3.add(lbl2);
 		
 		JLabel lbl4 = new JLabel("4");
 		lbl4.setIcon(new ImageIcon(LottoCMR.class.getResource("/image/04.png")));
-		lbl4.setBounds(333, 67, 20, 28);
+		lbl4.setBounds(331, 67, 20, 28);
 		panel_3.add(lbl4);
 		
 		JLabel lbl5 = new JLabel("5");
 		lbl5.setIcon(new ImageIcon(LottoCMR.class.getResource("/image/05.png")));
-		lbl5.setBounds(353, 67, 20, 28);
+		lbl5.setBounds(355, 67, 20, 28);
 		panel_3.add(lbl5);
 		
 		JLabel lbl6 = new JLabel("6");
@@ -73,6 +72,10 @@ public class LottoCMR extends JFrame {
 		lbl7.setIcon(new ImageIcon(LottoCMR.class.getResource("/image/07.png")));
 		lbl7.setBounds(404, 67, 20, 28);
 		panel_3.add(lbl7);
+		
+		JLabel lbl1_1 = new JLabel("1");
+		lbl1_1.setBounds(263, 110, 20, 28);
+		panel_3.add(lbl1_1);
 		
 		JLabel paper = new JLabel("New label");
 		paper.setIcon(new ImageIcon(LottoCMR.class.getResource("/image/paper.png")));
