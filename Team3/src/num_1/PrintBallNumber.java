6f54b795293645c1fdaadae92ac1b1d01b2a2b40
Jisu-Seong <sjangela0615@gmail.com>
2023-05-31 09:42:04 +0900

    이미지 45개 넣는식 조금수정

--- Team3/src/num_1/PrintBallNumber.java
+++ Team3/src/num_1/PrintBallNumber.java
@@ -6,21 +6,18 @@ import javax.swing.ImageIcon;
 
 class PrintBallNumber {
 	ImageIcon[] numImg = new ImageIcon[45];
-//	Image circle10 = new ImageIcon(LotteryConfirmation.class.getResource("/image/ten.png")).getImage();
-//	Image circle20 = new ImageIcon(LotteryConfirmation.class.getResource("/image/twenty.png")).getImage();
-//	Image circle30 = new ImageIcon(LotteryConfirmation.class.getResource("/image/thirty.png")).getImage();
-//	Image circle40 = new ImageIcon(LotteryConfirmation.class.getResource("/image/forty.png")).getImage();
-//	Image circle50 = new ImageIcon(LotteryConfirmation.class.getResource("/image/fifty.png")).getImage();
-//	Image auto = new ImageIcon(LotteryConfirmation.class.getResource("/image/auto.png")).getImage();
-	
-	
+	ImageIcon circle10 = new ImageIcon(PrintBallNumber.class.getResource("/image/ten.png"));
+	ImageIcon circle20 = new ImageIcon(PrintBallNumber.class.getResource("/image/twenty.png"));
+	ImageIcon circle30 = new ImageIcon(PrintBallNumber.class.getResource("/image/thirty.png"));
+	ImageIcon circle40 = new ImageIcon(PrintBallNumber.class.getResource("/image/forty.png"));
+	ImageIcon circle50 = new ImageIcon(PrintBallNumber.class.getResource("/image/fifty.png"));
+	ImageIcon auto = new ImageIcon(PrintBallNumber.class.getResource("/image/auto.png"));
+
 	public PrintBallNumber() {
 		for (int i = 0; i < numImg.length; i++) {
-			numImg[i] = new ImageIcon(numImg[i+1]+".png");
+//			numImg[i] = new ImageIcon(numImg[i+1]+".png");
+			String str = String.valueOf((i + 1));
+			numImg[i] = new ImageIcon(PrintBallNumber.class.getResource("/image/" + str + ".png"));
 		}
 	}
-	
-	
-	
-
 }
