6aaf22b8c8845ca18c65ec29e91b61630de91808
yxxndev <yxxn.dev@gmail.com>
2023-06-01 15:18:04 +0900

    텍스트 경고창 문구 수정

--- Team3/src/num_1/LottoCMR.java
+++ Team3/src/num_1/LottoCMR.java
@@ -304,18 +304,18 @@ public class LottoCMR extends JFrame {
 
 			if (markedNum > 0 && markedNum < 6) { // 조건 마다 메시지 한 줄 씩 추가
 				if (message.equals("")) {
-					message += "[" + keys + "] 복권의 번호 입력이 " + (fullNum - markedNum) + "개 부족합니다.";
+					message +=  "[!] 복권의 번호는 [총 6개]가 되어야 합니다.\n번호가 부족한 게임: " + "[" + keys + "] ";
 				} else {
-					message += "\n[" + keys + "] 복권의 번호 입력이 " + (fullNum - markedNum) + "개 부족합니다.";
+					message +=  "[" + keys + "] ";
 				}
 			}
 
 		}
 		if (finishMarking == 0) {
 			if (message.equals("")) {
-				message += "최소 한 개 이상의 게임이 선택되어야 합니다.";
+				message += "[!] 최소 한 개 이상의 게임이 선택되어야 합니다.";
 			} else {
-				message += "\n최소 한 개 이상의 게임이 선택되어야 합니다.";
+				message += "\n[!] 최소 한 개 이상의 게임이 선택되어야 합니다.";
 			}
 		}
 
