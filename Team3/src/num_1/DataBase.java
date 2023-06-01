951b7c1256e6cf504c260ed2b492267b094e4ea9
Jisu-Seong <sjangela0615@gmail.com>
2023-06-01 16:51:17 +0900

    mapAuto, mapCopy 추가

--- Team3/src/num_1/DataBase.java
+++ Team3/src/num_1/DataBase.java
@@ -4,24 +4,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 
 public class DataBase {
 	Map<String, ArrayList<UserSelectNum>> map = new HashMap();
-	Map<String, JLabel[]> mapCMR = new HashMap();
-
+	Map<String, JLabel[]> mapLbls = new HashMap();
+	Map<String, JButton> mapAuto = new HashMap();
+	Map<String, JButton> mapCopy = new HashMap();
+	
 	public DataBase() {
 		map.put("A", new ArrayList<UserSelectNum>());
 		map.put("B", new ArrayList<UserSelectNum>());
 		map.put("C", new ArrayList<UserSelectNum>());
 		map.put("D", new ArrayList<UserSelectNum>());
 		map.put("E", new ArrayList<UserSelectNum>());
-
-		mapCMR.put("A", new JLabel[45]);
-		mapCMR.put("B", new JLabel[45]);
-		mapCMR.put("C", new JLabel[45]);
-		mapCMR.put("D", new JLabel[45]);
-		mapCMR.put("E", new JLabel[45]);
+		
+		mapAuto.put("A", new JButton());
+		mapAuto.put("B", new JButton());
+		mapAuto.put("C", new JButton());
+		mapAuto.put("D", new JButton());
+		mapAuto.put("E", new JButton());
+		
+		mapCopy.put("A", new JButton());
+		mapCopy.put("B", new JButton());
+		mapCopy.put("C", new JButton());
+		mapCopy.put("D", new JButton());
+		mapCopy.put("E", new JButton());
+		
 		
 //		add();
 
