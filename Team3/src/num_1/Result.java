2c29135ef0fb0b4c2d05360d98cece68d7868772
yxxndev <yxxn.dev@gmail.com>
2023-06-02 15:32:12 +0900

    이미지 수정

--- Team3/src/num_1/Result.java
+++ Team3/src/num_1/Result.java
@@ -621,6 +621,7 @@ class Result extends JFrame {
 		JButton btn1 = new JButton(img1);
 		btn1.setRolloverIcon(img3);
 		btn1.setBorderPainted(false);
+		btn1.setFocusPainted(false);
 		btn1.setContentAreaFilled(false);
 		btn1.setBounds(130, 645, 150, 70);
 
